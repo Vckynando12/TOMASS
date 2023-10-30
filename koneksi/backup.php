f61753fb757d8eece146aa08aa0c614992c18666
Vckynando12 <vickynando12@gmail.com>
2023-10-30 15:21:41 +0700

    menambahkan backup

---
+++ koneksi/backup.php
@@ -0,0 +1,10 @@
+<?php
+
+    $con = mysqli_connect("localhost", "root", "", "tomass");
+
+    //Check connection
+    if (mysqli_connect_errno()) {
+        echo "Tidak terhubung ke MySQL".mysqli_connect_errno();
+        exit();
+    }
+?>
