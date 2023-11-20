1af4fdd2fed614ba32468e50e84c8dc5fcc2ff91
malemnaswaritarigan <malemnaswaritarigan@gmail.com>
2023-11-20 23:15:53 +0700

    add: proses checkout

--- proses/proses_checkout.php
+++ proses/proses_checkout.php
@@ -21,15 +21,11 @@ if ($keranjang) {
                                                    INNER JOIN barang b ON dk.id_barang = b.id_barang
                                                    WHERE dk.id_keranjang = '$id_keranjang'");
     $detail_keranjang = mysqli_fetch_all($query_detail_keranjang, MYSQLI_ASSOC);
-
-    // Proses checkout
     $nama = mysqli_real_escape_string($con, $_POST['nama']);
     $provinsi = mysqli_real_escape_string($con, $_POST['provinsi']);
     $kota = mysqli_real_escape_string($con, $_POST['kota']);
     $alamat = mysqli_real_escape_string($con, $_POST['alamat']);
     $kode_pos = mysqli_real_escape_string($con, $_POST['kode_pos']);
-
-    // Insert the "nama" into the detail_penjualan table
     $query_insert_penjualan = mysqli_query($con, "INSERT INTO penjualan (id_user, tanggal_penjualan, total_penjualan) VALUES ('$id_user_login', NOW(), '$total_harga')");
     $id_penjualan = mysqli_insert_id($con); 
 
@@ -44,10 +40,8 @@ if ($keranjang) {
     mysqli_query($con, "INSERT INTO pembayaran (id_pembayaran, tgl_bayar, total_bayar, status) VALUES ('$id_penjualan', NOW(), '$total_harga', '0')");
     mysqli_query($con, "DELETE FROM keranjang WHERE id_keranjang = '$id_keranjang'");
     mysqli_query($con, "DELETE FROM detail_keranjang WHERE id_keranjang = '$id_keranjang'");
-
     header("Location: ../proses/struk.php?id_penjualan=$id_penjualan");
     exit();
-
 } else {
     echo "Keranjang belanja Anda kosong.";
 }
