126caa5c15b3b9742849b2918d6ebbfb62349447
Niswahtrs <niswahayun8@gmail.com>
2023-12-02 03:24:44 +0700

    modified: home.php

--- public/home.php
+++ public/home.php
@@ -5,26 +5,131 @@ $query_products = mysqli_query($con, "SELECT * FROM barang WHERE nama_barang LIK
 $products = mysqli_fetch_all($query_products, MYSQLI_ASSOC);
 ?>
 
-<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-<link rel="stylesheet" href="../assets/css/style.css">
+<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css"> 
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+<link href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" rel="stylesheet"/>
+<link rel="stylesheet" href="../assets/css/card.css">
+<style>
+    .box-affix {
+            padding: 15px;
+            background-color: #f8f9fa;
+            border: 1px solid #dee2e6;
+            border-radius: 5px;
+            margin-bottom: 10px;
+        }
+        .bg-affix2 {
+            background-color: #e2e3e5;
+        }
+        .line1 {
+            width: 20px;
+            height: 3px;
+            background-color: #007bff;
+            margin-bottom: 10px;
+        }
+        .wrap-affix {
+        position: relative;
+        top: 170px;
+        padding: 8px 32px;
+        }
+
+        .bg-affix1 {
+        background: rgb(98, 0, 73);
+        background: rgb(57, 0, 98);
+        background: linear-gradient(
+            90deg,
+            rgb(98 0 84) 0%,
+            rgba(158, 82, 150, 1) 100%
+        );
+        }
+
+        .bg-affix2 {
+        background: rgb(98, 0, 73);
+        background: rgb(57, 0, 98);
+        background: linear-gradient(
+            90deg,
+            rgba(57, 0, 98, 1) 0%,
+            rgba(158, 82, 150, 1) 100%
+        );
+        }
+
+        .box-affix {
+        display: inline-block;
+        width: 216px;
+        height: 64px;
+        padding: 6px 8px;
+        margin-right: 10px;
+        border: none;
+        border-radius: 8px;
+        color: #fff;
+        }
+
+        .line1 {
+        background-color: #fff;
+        height: 4px;
+        width: 40px;
+        }
+
+        .card {
+        box-shadow: rgba(49, 53, 59, 0.12) 0px 1px 6px 0px;
+        border-radius: 12px;
+        }
+
+        .card-body {
+        padding: 10px;
+        }
+
+        .affix {
+        width: 100%;
+        background-color: white;
+        top: 110px;
+        z-index: 9999999;
+        left: 0;
+        height: 76px;
+        padding-left: 32px;
+        }
+
+        .btn-green {
+        color: #03ac0e;
+        border: 1px solid #03ac0e;
+        background-color: transparent;
+        padding: 10px 70px;
+        font-size: 20px;
+        font-weight: bold;
+        }
+</style>
 <div class="container mt-5">
+    <div class="position-relative mr-3 box-affix bg-affix2">
+        <div class="line1"></div>
+        <h4>For You</h4>
+    </div>
     <div class="row">
-        <?php foreach ($products as $product) : ?>
-            <div class="col-md-4 mb-4">
-                <div class="card" style="width: 200px; margin-bottom: 20px;">
-                    <?php
-                    $imageData = base64_encode($product['gambar']);
-                    $imageSrc = 'data:image/jpeg;base64,' . $imageData;
-                    ?>
-                    <img src="<?= $imageSrc; ?>" class="card-img-top custom-image" style="height: 200px; width: 200px;" alt="<?= $product['nama_barang']; ?>">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $product['nama_barang']; ?></h5>
-                        <p class="card-text">Price: <?= $product['harga_jual']; ?></p>
-                        <p class="card-text">Stock: <?= $product['stok']; ?> <?= $product['satuan']; ?></p>
-                        <button class="btn btn-primary add-to-cart" data-product-id="<?= $product['id_barang']; ?>">Add to Cart</button>
+        <?php foreach ($products as $product): ?>
+            <div class="col-md-3 mb-3">
+                <div class="wsk-cp-product">
+                    <div class="wsk-cp-img">
+                        <?php
+                        $imageData = base64_encode($product['gambar']);
+                        $imageSrc = 'data:image/jpeg;base64,' . $imageData;
+                        ?>
+                        <img src="<?= $imageSrc; ?>" alt="<?= $product['nama_barang']; ?>" class="img-responsive custom-image" />
+                    </div>
+                    <div class="wsk-cp-text">
+                        <div class="category">
+                            <span><?= $product['nama_barang']; ?></span>
+                        </div>
+                        <div class="description-prod">
+                            <p class="mt-2">Stock: <?= $product['stok'] . ' ' . $product['satuan']; ?></p>
+                        </div>
+                        <div class="card-footer">
+                            <div class="wcf-left"><span class="price">Rp. <?= $product['harga_jual']; ?></span></div>
+                            <div class="wcf-right"><a href="#" class="buy-btn add-to-cart" data-product-id="<?= $product['id_barang']; ?>"><i class="zmdi zmdi-shopping-basket"></i></a></div>
+                        </div>
                     </div>
                 </div>
             </div>
+
         <?php endforeach; ?>
     </div>
 </div>
