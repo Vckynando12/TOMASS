305504275b41082feca114ba34c1c5a821560e35
berru22 <bellaervina29@gmail.com>
2023-12-12 11:06:18 +0700

    fix: header user

--- proses/header.php
+++ proses/header.php
@@ -1,17 +1,33 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
+session_start();
 
 include("../koneksi/koneksi.php");
-if (isset($_SESSION['username']) && is_array($_SESSION['username'])) {
-    $user = $_SESSION['username'];
-    $query_keranjang = mysqli_query($con, "SELECT * FROM keranjang WHERE id_user = '$user[id_user]'");
+
+$user = isset($_SESSION['username']) && is_array($_SESSION['username']) ? $_SESSION['username'] : null;
+$jumlah_barang_keranjang = 0;
+
+if ($user) {
+    $query_keranjang = mysqli_query($con, "SELECT * FROM keranjang WHERE id_user = '{$user['id_user']}'");
     $data_keranjang = mysqli_fetch_assoc($query_keranjang);
-    $jumlah_barang_keranjang = 0;
+
+    $ppQuery = "SELECT pp FROM user WHERE id_user = '{$user['id_user']}'";
+    $ppResult = mysqli_query($con, $ppQuery);
+
+    if ($ppResult) {
+        $ppData = mysqli_fetch_assoc($ppResult);
+
+        if ($ppData['pp']) {
+            $ppImage = base64_encode($ppData['pp']);
+        } else {
+            $ppImage = base64_encode(file_get_contents('https://bootdey.com/img/Content/avatar/avatar1.png'));
+        }
+    } else {
+        $ppImage = base64_encode(file_get_contents('https://bootdey.com/img/Content/avatar/avatar1.png'));
+    }
+
 
     if ($data_keranjang) {
-        $query_detail_keranjang = mysqli_query($con, "SELECT SUM(jumlah) AS total_barang FROM detail_keranjang WHERE id_keranjang = '$data_keranjang[id_keranjang]'");
+        $query_detail_keranjang = mysqli_query($con, "SELECT SUM(jumlah) AS total_barang FROM detail_keranjang WHERE id_keranjang = '{$data_keranjang['id_keranjang']}'");
         $data_detail_keranjang = mysqli_fetch_assoc($query_detail_keranjang);
         $jumlah_barang_keranjang = $data_detail_keranjang['total_barang'];
     }
@@ -21,146 +37,134 @@ if (isset($_SESSION['username']) && is_array($_SESSION['username'])) {
 <html lang="en">
 
 <head>
-    <title>TOMASS</title>
+    <title>Tomass</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="../assets/image/logo.ico">
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <!-- <link rel="stylesheet" href="../css/style.css"> -->
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../assets/js/script.js"></script>
+    <link rel="stylesheet" href="../assets/css/nav.css">
 
-    <style>
-    .navbar {
-        box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.3);
-        background-color: #fff;
-    }
-
-    .navbar-brand img {
-        max-height: 40px;
-    }
-
-    .navbar-nav .nav-link {
-        color: #333;
-    }
-
-    .navbar-nav .nav-link:hover {
-        color: #007bff;
-    }
-
-    .form-control {
-        border: 1px solid #ced4da;
-        border-radius: 5px;
-        padding: 8px;
-    }
-
-    .btn-primary {
-        border-radius: 5px;
-        margin-left: 10px;
-    }
-
-    #userActions .nav-item {
-        margin-right: 15px;
-    }
-
-    #cartIcon img {
-        height: 20px;
-        margin-right: 5px;
-    }
-
-    .dropdown-menu {
-        border: 1px solid rgba(0, 0, 0, 0.1);
-        box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
-    }
-    .header-logo {
-        display: flex;
-        align-items: center;
-    }
-
-    .logo ul {
-        margin: 0;
-        padding: 0;
-        list-style: none;
-    }
-
-    .logo img {
-        max-height: 40px;
-        margin-right: 10px;
-        margin-left: 10px;
-    }
-</style>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
+    <script src="https://kit.fontawesome.com/c79e220d71.js" crossorigin="anonymous"></script>
 
 </head>
 
-
 <body>
-    <!-- nav Logo-->
-    <nav class="navbar navbar-expand-lg ftco_navbar ftco-navbar-light mb-2" id="ftco-navbar">
-        <div class="header-logo">
-            <div class="logo">
-                <ul>
-                    <a href="../public/index.php">
-                        <img src="../assets/image/logoHeader.png" alt="">
-                    </a>
-                </ul>
+<nav class="navbar navbar-expand-lg bg-dark navbar-light d-none d-lg-block" id="templatemo_nav_top">
+        <div class="container text-light">
+            <div class="w-100 d-flex justify-content-between">
+                <div>
+                    <i class="fa fa-envelope mx-2"></i>
+                    <a class="navbar-sm-brand text-light text-decoration-none" href="mailto:info@company.com">niswahayun8@gmail.com</a>
+                    <i class="fa fa-phone mx-2"></i>
+                    <a class="navbar-sm-brand text-light text-decoration-none" href="https://api.whatsapp.com/send?phone=6285781937953">0857-8193-7953</a>
+                </div>
+                <div>
+                    <a class="text-light" href="" target="_blank" rel="sponsored"><i class="fab fa-facebook-f fa-sm fa-fw me-2"></i></a>
+                    <a class="text-light" href="" target="_blank"><i class="fab fa-instagram fa-sm fa-fw me-2"></i></a>
+                    <a class="text-light" href="" target="_blank"><i class="fab fa-twitter fa-sm fa-fw me-2"></i></a>
+                    <a class="text-light" href="" target="_blank"><i class="fab fa-linkedin fa-sm fa-fw"></i></a>
+                </div>
             </div>
         </div>
+    </nav>
 
-        <!-- nav Search-->
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <form class="d-flex mx-auto" action="index.php" method="GET">
-                <input class="form-control me-2" type="search" name="query" placeholder="Search" aria-label="Search"
-                    style="width: 500px; border: 1px solid #ced4da; border-radius: 5px; padding: 8px;">
-                <button class="btn btn-outline-success" style="border-radius: 5px; margin-left: 10px;" type="submit"><svg
-                        xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
-                        class="bi bi-search" viewBox="0 0 15 15">
-                        <path
-                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                    </svg>
-                </button>
-            </form>
-        </div>
+    <nav class="navbar navbar-expand-lg navbar-light shadow">
+        <div class="container d-flex justify-content-between align-items-center">
 
-        <!-- nav Menu-->
-        <div class="collapse navbar-collapse" id="ftco-nav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active"><a href="../public/index.php" class="nav-link">Home</a></li>
-                <li class="nav-item"><a href="../public/about.php" class="nav-link">About</a></li>
-                <ul class="navbar-nav ms-auto" id="userActions">
-                    <?php if (isset($_SESSION['username'])): ?>
-                        <li class="nav-item" id="cartIcon">
-                            <a class="nav-link" href="../proses/keranjang.php">
-                                <img src="../assets/image/cart.png" alt="Keranjang" style="height: 20px;">
-                                <?php if ($jumlah_barang_keranjang > 0): ?>
-                                    <span class="badge bg-danger">
-                                        <?= htmlspecialchars($jumlah_barang_keranjang); ?>
-                                    </span>
-                                <?php endif; ?>
-                            </a>
+            <a href="../public/index.php">
+                <img src="../assets/image/logoHeader.png" alt="">
+            </a>
+
+            <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
+                <div class="flex-fill">
+                    <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="../public/home3.php">Home</a>
                         </li>
-                        <li class="nav-item dropdown" id="accountDropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="accountText" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <?= htmlspecialchars($user['username']); ?>
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="accountDropdown">
-                                <a class="dropdown-item" href="../proses/riwayat_pesanan.php">Pesanan saya</a>
-                                <a class="dropdown-item" href="../proses/logout_proses.php">Logout</a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../public/index.php">Shop</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../public/about.php">About</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="navbar align-self-center d-flex my-2">
+                    <div class="d-lg-none flex-sm-fill mt-3 mb-4 col-7 col-sm-auto pr-3">
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="inputMobileSearch" placeholder="Search ...">
+                            <div class="input-group-text">
+                                <i class="fa fa-fw fa-search"></i>
                             </div>
-                        </li>
+                        </div>
+                    </div>
+                    <a class="nav-icon d-none d-lg-inline" href="#" data-bs-toggle="modal" data-bs-target="#templatemo_search">
+                        <i class="fa fa-fw fa-search text-dark mr-2"></i>
+                    </a>
+                    <a class="nav-icon position-relative text-decoration-none" href="../proses/keranjang.php">
+                        <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
+                        <?php if ($jumlah_barang_keranjang > 0): ?>
+                            <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark"><?= $jumlah_barang_keranjang; ?></span>
+                        <?php endif; ?>
+                    </a>
+                    <?php if ($user): ?>
+                        <a class="nav-icon position-relative text-decoration-none" href="#" id="accountDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <?php if ($ppImage): ?>
+                                <img src="data:image/jpeg;base64,<?php echo $ppImage; ?>" alt="Profile Picture" class="rounded-circle" width="30" height="30">
+                            <?php else: ?>
+                                <i class="fa fa-fw fa-user text-dark mr-3"></i>
+                            <?php endif; ?>
+                            <?= htmlspecialchars($user['username']); ?>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="accountDropdown">
+                            <a class="dropdown-item" href="../proses/riwayat_pesanan.php">Pesanan saya</a>
+                            <a class="dropdown-item" href="../proses/akun.php">setting</a>
+                            <a class="dropdown-item" href="../proses/logout_proses.php">Logout</a>
+                        </div>
                     <?php else: ?>
-                        <li class="nav-item dropdown" id="accountDropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="accountText" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Login | Register
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="accountDropdown">
-                                <a class="dropdown-item" href="../public/login.php">Login</a>
-                                <a class="dropdown-item" href="../public/login.php">Register</a>
-                            </div>
-                        </li>
+                        <a class="nav-icon position-relative text-decoration-none" href="#" id="accountDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-fw fa-user text-dark mr-3"></i>
+                            <?php if ($jumlah_barang_keranjang > 0): ?>
+                                <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark"><?= $jumlah_barang_keranjang; ?></span>
+                            <?php endif; ?>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="accountDropdown">
+                            <a class="dropdown-item" href="../public/login.php">Login</a>
+                            <a class="dropdown-item" href="../public/login.php">Register</a>
+                        </div>
                     <?php endif; ?>
-                </ul>
+                </div>
+            </div>
         </div>
     </nav>
+    <!-- Close Header -->
+
+    <!-- Modal -->
+    <div class="modal fade bg-white" id="templatemo_search" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="w-100 pt-1 mb-5 text-right">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="index.php" method="GET" class="modal-content modal-body border-0 p-0">
+                <div class="input-group mb-2">
+                    <input type="text" class="form-control" id="inputModalSearch" name="query" placeholder="Search ...">
+                    <button type="submit" class="input-group-text bg-success text-light">
+                        <i class="fa fa-fw fa-search text-white"></i>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+<!-- Include jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- Include Popper Core library -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+
+    <script src="../assets/jquery-1.11.0.min.js"></script>
+    <script src="../assets/js/script.js"></script>
