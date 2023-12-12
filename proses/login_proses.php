5c69e44166809b700b7e78f1ddb0216f7e8c310e
berru22 <bellaervina29@gmail.com>
2023-12-12 11:07:41 +0700

    fix: login proses gmeial

--- proses/login_proses.php
+++ proses/login_proses.php
@@ -10,25 +10,30 @@ if (isset($_POST['loginbtn'])){
     $data = mysqli_fetch_assoc($query);
 
     if($countdata > 0){
-        $hashed = password_hash($data['password'], PASSWORD_BCRYPT);
-        if(password_verify($password, $hashed)){
-            $_SESSION['username'] = $data;
-            $_SESSION['login'] = true;
-            $_SESSION['user_id'] = $data['id_user'];
-            $_SESSION['level'] = $data['level'];
+        if($data['verified'] == 1){
+            if(password_verify($password, $data['password'])){
+                $_SESSION['username'] = $data;
+                $_SESSION['login'] = true;
+                $_SESSION['user_id'] = $data['id_user'];
+                $_SESSION['level'] = $data['level'];
 
-            if($data['level'] == 1){
-                header('location: ../admin/index.php');
-                exit();
+                if($data['level'] == 1){
+                    header('location: ../admin/index.php');
+                    exit();
+                }
+                elseif($data['level'] == 2) {
+                    header('location: ../public/index.php');
+                    exit();
+                }
             }
-            elseif($data['level'] == 2) {
-                header('location: ../public/index.php');
-                exit();
+            else{
+                $password_error = true;
+                $_SESSION['message'] = "Password salah, silakan coba lagi.";
+                $_SESSION['redirect'] = true;
             }
         }
         else{
-            $password_error = true;
-            $_SESSION['message'] = "Password salah, silakan coba lagi.";
+            $_SESSION['message'] = "Akun belum terverifikasi. Silakan verifikasi akun Anda melalui email terlebih dahulu.";
             $_SESSION['redirect'] = true;
         }
     }
