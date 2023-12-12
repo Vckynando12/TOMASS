d2de467fc04a3dfb4033797060ef32ff4c986f43
Niswahtrs <niswahayun8@gmail.com>
2023-12-12 10:02:05 +0700

    fix: penghapusan barang

--- function/hapus.php
+++ function/hapus.php
@@ -1,24 +1,38 @@
 <?php
-require '../koneksi/koneksi.php';
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-if (!isset($_SESSION['login'])) {
-    header('location: ../public/login.php');
+session_start();
+if (!isset($_SESSION['login']) || $_SESSION['level'] != 1) {
+    echo "<script>window.location.href = '../public/login.php';</script>";
     exit();
 }
+
+require '../koneksi/koneksi.php';
+
 // HAPUS BARANG
+
 if (isset($_GET['hapus_kode'])) {
+
     $id_barang_hapus = $_GET['hapus_kode'];
 
+
+
     $delete_query = "DELETE FROM barang WHERE id_barang = '$id_barang_hapus'";
+
     $delete_result = mysqli_query($con, $delete_query);
 
+
+
     if ($delete_result) {
+
         header("Location: ../admin/m_barang.php");
+
         exit();
+
     } else {
+
         echo "Gagal menghapus barang.";
+
     }
+
 }
+
 ?>
