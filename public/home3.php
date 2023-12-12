9b577547ccff77f7dd6b85da0018d917de556487
berru22 <bellaervina29@gmail.com>
2023-12-12 11:19:51 +0700

    add: tampilan utama user

---
+++ public/home3.php
@@ -0,0 +1,456 @@
+<?php
+
+include '../proses/header.php';
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <meta name="author" content="Untree.co">
+
+
+
+  <meta name="description" content="" />
+
+  <meta name="keywords" content="bootstrap, bootstrap4" />
+
+
+
+		<!-- Bootstrap CSS -->
+
+		<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+		<link href="../assets/css/tiny-slider.css" rel="stylesheet">
+
+		<link href="../assets/css/home.css" rel="stylesheet">
+
+		<title>Tomass eCommerce - Pusat Belanja Bahan Baku Kue Terlengkap</title>
+
+	</head>
+
+
+
+	<body>
+
+
+
+
+
+
+
+		<!-- Start Product Section -->
+		<div class="product-section">
+			<div class="container">
+				<div class="row">
+
+					<!-- Start Column 1 -->
+					<div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
+                <h2 class="mb-4 section-title">Bahan Baku Kue Berkualitas Tinggi</h2>
+                <p class="mb-4">Temukan bahan baku kue terbaik yang dihasilkan dengan material berkualitas tinggi. Tomass menyediakan pilihan terbaik untuk memastikan kreasi kue Anda menjadi luar biasa.</p>
+                <p><a href="index.php" class="btn btn-outline-success">Jelajahi</a></p>
+					</div> 
+					<!-- End Column 1 -->
+
+					<!-- Start Column 2 -->
+					<div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+						<span class="product-item">
+							<img src="../assets/image/produk2.png" class="img-fluid product-thumbnail">
+							<h3 class="product-title">Tepung Beras Rosebrand</h3>
+							<strong class="product-price">Rp. 6,500</strong>
+
+							<a class="icon-cross" href="index.php">
+								<img src="../assets/image/cross.svg" class="img-fluid">
+							</a>
+						</span>
+					</div> 
+					<!-- End Column 2 -->
+
+					<!-- Start Column 3 -->
+					<div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+						<span class="product-item">
+							<img src="../assets/image/produk3.png" class="img-fluid product-thumbnail">
+							<h3 class="product-title">Pewarna Makanan</h3>
+							<strong class="product-price">Rp. 6,000</strong>
+
+							<a href="index.php" class="icon-cross">
+								<img src="../assets/image/cross.svg" class="img-fluid">
+							</a>
+						</span>
+					</div>
+					<!-- End Column 3 -->
+
+					<!-- Start Column 4 -->
+					<div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+						<span class="product-item">
+							<img src="../assets/image/produk1.png" class="img-fluid product-thumbnail">
+							<h3 class="product-title">Blue Band Margarine</h3>
+							<strong class="product-price">Rp. 11,000</strong>
+
+							<a href="index.php" class="icon-cross">
+								<img src="../assets/image/cross.svg" class="img-fluid">
+							</a>
+						</span>
+					</div>
+					<!-- End Column 4 -->
+
+				</div>
+			</div>
+		</div>
+		<!-- End Product Section -->
+
+
+		<!-- Start Why Choose Us Section -->
+
+<div class="why-choose-section">
+
+    <div class="container">
+
+        <div class="row justify-content-between">
+
+            <div class="col-lg-6">
+
+                <h2 class="section-title">Mengapa Memilih Tomass</h2>
+
+                <p>Tomass hadir dengan berbagai keunggulan untuk memenuhi kebutuhan Anda. Berikut adalah alasan-alasan mengapa Anda harus memilih Tomass sebagai mitra baking Anda:</p>
+
+
+
+                <div class="row my-5">
+
+                    <div class="col-6 col-md-6">
+
+                        <div class="feature">
+
+                            <div class="icon">
+
+                                <img src="../assets/image/jam.svg" alt="Image" class="img-fluid">
+
+                            </div>
+
+                            <h3>Pemesanan yg cepat dan praktis</h3>
+
+                            <p>Tomass hadir untuk memudahkan Anda mendapatkan bahan kue berkualitas dengan proses yang efisien dan tanpa ribet.</p>
+
+                        </div>
+
+                    </div>
+
+
+
+                    <div class="col-6 col-md-6">
+
+                        <div class="feature">
+
+                            <div class="icon">
+
+                                <img src="../assets/image/bag.svg" alt="Image" class="img-fluid">
+
+                            </div>
+
+                            <h3>Mudah untuk Berbelanja</h3>
+
+                            <p>Tomass membuat pengalaman berbelanja Anda menjadi mudah dan menyenangkan dengan antarmuka yang user-friendly.</p>
+
+                        </div>
+
+                    </div>
+
+
+
+                    <div class="col-6 col-md-6">
+
+                        <div class="feature">
+
+                            <div class="icon">
+
+                                <img src="../assets/image/support.svg" alt="Image" class="img-fluid">
+
+                            </div>
+
+                            <h3>Dukungan 24/7</h3>
+
+                            <p>Tim customer service Tomass siap membantu Anda 24/7 untuk menjawab pertanyaan dan memberikan bantuan yang Anda butuhkan.</p>
+
+                        </div>
+
+                    </div>
+
+
+
+                    <div class="col-6 col-md-6">
+
+                        <div class="feature">
+
+                            <div class="icon">
+
+                                <img src="../assets/image/return.svg" alt="Image" class="img-fluid">
+
+                            </div>
+
+                            <h3>Pengembalian Tanpa Masalah</h3>
+
+                            <p>Jika ada ketidakpuasan, Tomass menawarkan kebijakan pengembalian tanpa masalah untuk kenyamanan Anda.</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+            <div class="col-lg-5">
+
+                <div class="img-wrap">
+
+                    <img src="../assets/image/toko1.jpeg" alt="Image" class="img-fluid">
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- End Why Choose Us Section -->
+
+
+
+
+
+		<!-- Start We Help Section -->
+
+<div class="we-help-section">
+
+    <div class="container">
+
+        <div class="row justify-content-between">
+
+            <div class="col-lg-7 mb-5 mb-lg-0">
+
+                <div class="imgs-grid">
+
+                    <div class="grid grid-1"><img src="../assets/image/toko.jpg" alt="Tomass - Toko Bahan Kue"></div>
+
+                    <div class="grid grid-2"><img src="../assets/image/toko3.jpg" alt="Tomass - Toko Bahan Kue"></div>
+
+                    <div class="grid grid-3"><img src="../assets/image/toko2.jpg" alt="Tomass - Toko Bahan Kue"></div>
+
+                </div>
+
+            </div>
+
+            <div class="col-lg-5 ps-lg-5">
+
+                <h2 class="section-title mb-4">Tomass - Toko Bahan Kue</h2>
+
+                <p>Tomass adalah toko bahan kue yang akan menjadikan kue Anda lebih nikmat. Kami menyediakan bahan baku berkualitas dan terjangkau secara lengkap. Dengan bahan berkualitas dari Tomass, kreasi kue Anda akan menjadi istimewa.</p>
+
+                <ul class="list-unstyled custom-list my-4">
+
+                    <li>Bahan baku berkualitas</li>
+
+                    <li>Harga terjangkau</li>
+
+                    <li>Varian lengkap</li>
+
+                    <li>Kelezatan yang tak tertandingi</li>
+
+                </ul>
+
+                <p><a href="#" class="btn btn-outline-success">Jelajahi</a></p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- End We Help Section -->
+
+
+
+
+
+		
+
+		<!-- Start Testimonial Slider -->
+
+		<div class="testimonial-section">
+
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-lg-7 mx-auto text-center">
+
+						<h2 class="section-title">Testimonials</h2>
+
+					</div>
+
+				</div>
+
+
+
+				<div class="row justify-content-center">
+
+					<div class="col-lg-12">
+
+						<div class="testimonial-slider-wrap text-center">
+
+
+
+							<div id="testimonial-nav">
+
+								<span class="prev" data-controls="prev"><span class="fa fa-chevron-left"></span></span>
+
+								<span class="next" data-controls="next"><span class="fa fa-chevron-right"></span></span>
+
+							</div>
+
+
+
+							<div class="testimonial-slider">
+
+								
+
+								<div class="item">
+
+									<div class="row justify-content-center">
+
+										<div class="col-lg-8 mx-auto">
+
+
+
+											<div class="testimonial-block text-center">
+
+												<blockquote class="mb-5">
+
+													<p>&ldquo;Coming soon&rdquo;</p>
+
+												</blockquote>
+
+
+
+												<!-- <div class="author-info">
+
+													<div class="author-pic">
+
+														<img src="images/person-1.png" alt="Maria Jones" class="img-fluid">
+
+													</div>
+
+													<h3 class="font-weight-bold">Maria Jones</h3>
+
+													<span class="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
+
+												</div> -->
+
+											</div>
+
+
+
+										</div>
+
+									</div>
+
+								</div> 
+
+								<!-- END item -->
+
+								<div class="item">
+
+									<div class="row justify-content-center">
+
+										<div class="col-lg-8 mx-auto">
+
+
+
+											<div class="testimonial-block text-center">
+
+												<blockquote class="mb-5">
+
+													<p>&ldquo;Coming soon&rdquo;</p>
+
+												</blockquote>
+
+
+
+												<!-- <div class="author-info">
+
+													<div class="author-pic">
+
+														<img src="images/person-1.png" alt="Maria Jones" class="img-fluid">
+
+													</div>
+
+													<h3 class="font-weight-bold">Maria Jones</h3>
+
+													<span class="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
+
+												</div> -->
+
+											</div>
+
+
+
+										</div>
+
+									</div>
+
+								</div> 
+
+								<!-- END item -->
+
+
+
+
+
+							</div>
+
+
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<!-- End Testimonial Slider -->
+
+
+
+		<script src="../assets/jquery-1.11.0.min.js"></script>
+
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+
+		<script src="../assets/js/tiny-slider.js"></script>
+
+		<script src="../assets/js/custom.js"></script>
+
+	</body>
+
+<?php include '../proses/footer.php'; ?>
+
+</html>
+
