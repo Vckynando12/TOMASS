7061304a3d75e2da290faf2f7d3c4315388ffefc
Niswahtrs <niswahayun8@gmail.com>
2023-12-12 10:04:43 +0700

    fix: penerimaan pesanan

--- function/terima_pesanan.php
+++ function/terima_pesanan.php
@@ -1,23 +1,38 @@
 <?php
-include("../koneksi/koneksi.php");
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-if (!isset($_SESSION['login'])) {
-    header('location: ../public/login.php');
+session_start();
+if (!isset($_SESSION['login']) || $_SESSION['level'] != 1) {
+    echo "<script>window.location.href = '../public/login.php';</script>";
     exit();
 }
+
+include("../koneksi/koneksi.php");
+
+
 if (isset($_POST['id_penjualan'])) {
+
     $idPenjualan = $_POST['id_penjualan'];
+
+
 
     $queryUpdateStatus = mysqli_query($con, "UPDATE pembayaran SET status = '1' WHERE id_pembayaran = '$idPenjualan'");
 
+
+
     if ($queryUpdateStatus) {
+
         echo "Pesanan dengan ID $idPenjualan berhasil diterima.";
+
     } else {
+
         echo "Gagal menerima pesanan.";
+
     }
+
 } else {
+
     echo "Parameter id_penjualan tidak ditemukan.";
+
 }
+
 ?>
+
