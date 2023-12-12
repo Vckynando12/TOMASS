ed729c8b109555e26bff1c959dc45987d1b09f76
berru22 <bellaervina29@gmail.com>
2023-12-12 11:05:34 +0700

    fix: footer

--- proses/footer.php
+++ proses/footer.php
@@ -1,67 +1,136 @@
 <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css"> 
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-<script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+
+
 <script src="https://kit.fontawesome.com/c79e220d71.js" crossorigin="anonymous"></script>
 
+
+
     <footer class="text-center text-lg-start text-dark" style="background-color: #ECEFF1">
+
       <section class="d-flex justify-content-between p-3 text-white" style="background-color: #80AB8C">
+
         <div class="me-5">
+
           <span>Terhubung dengan kami di jaringan sosial:</span>
+
         </div>
+
         <div>
+
           <a href="https://www.instagram.com/berrusaha.team/" class="text-white me-4">
+
             <i class="fab fa-instagram"></i>
+
           </a>
+
         </div>
+
       </section>
+
   
+
       <section class="">
+
         <div class="container text-center text-md-start mt-5">
+
           <div class="row mt-3">
+
             <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
               <h6 class="text-uppercase fw-bold">Tomass</h6>
+
               <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px"/>
+
               <p>
+
               Memberikan Pelayanan Terbaik dengan Kualitas Terpercaya
+
               </p>
+
             </div>
 
+
+
             <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
               <h6 class="text-uppercase fw-bold">Menu</h6>
+
               <hr
+
                   class="mb-4 mt-0 d-inline-block mx-auto"
+
                   style="width: 60px; background-color: #7c4dff; height: 2px"
+
                   />
-              <p>
-                <a href="../public/index.php" class="text-dark">Home</a>
+
+		<p>
+
+                <a href="../public/home3.php" class="text-dark">Home</a>
+
               </p>
+
+
               <p>
+
+                <a href="../public/index.php" class="text-dark">Shop</a>
+
+              </p>
+
+              <p>
+
                 <a href="../proses/riwayat_pesanan.php" class="text-dark">Pesanan saya</a>
+
               </p>
+
               <p>
+
                 <a href="../public/about.php" class="text-dark">About</a>
+
               </p>
               <p>
                 <a href="../proses/keranjang.php" class="text-dark">Cart</a>
               </p>
+
             </div>
 
+
+
             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
               <h6 class="text-uppercase fw-bold">Contact</h6>
+
               <hr
+
                   class="mb-4 mt-0 d-inline-block mx-auto"
+
                   style="width: 60px; background-color: #7c4dff; height: 2px"
+
                   />
+
               <p><i class="fas fa-home mr-3"></i> Jl.Raya Kesamben Desa Kesamben, Kecamatan Kesamben, Kabupaten Jombang</p>
+
               <p><i class="fas fa-envelope mr-3"></i> niswahayun8@gmail.com</p>
+
               <p><i class="bi bi-whatsapp mr-3"></i><a href="https://wa.me/+6285781937953"> + 62 8578 1937 953 </a></p>
+
             </div>
+
           </div>
+
         </div>
+
       </section>
+
       <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+
         © 2023 Copyright:
+
         <a class="text-dark" href="https://www.instagram.com/berrusaha.team/">BerrusahaTeam</a>
+
       </div>
+
     </footer>
