d1768bc10753ee68b199841f702f0ab5bf40002c
Vicky <vickynando12@gmal.com>
2023-12-12 09:48:27 +0700

    fix: koneksi

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,8 +1,5 @@
 <?php
-
-    $con = mysqli_connect("localhost", "root", "", "tomass");
-
-    //Check connection
+    $con = mysqli_connect("localhost", "mifbmyh1_tomass", "WSImif2023", "mifbmyh1_tomass");
     if (mysqli_connect_errno()) {
         echo "Tidak terhubung ke MySQL".mysqli_connect_errno();
         exit();
